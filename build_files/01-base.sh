53a021acf2f10a1e34e65540060c7d494d9b1ea6
Ahmed Adan <ahmed.adan@gmail.com>
2026-01-25 11:17:25 -0500

    feat: add Razorfin branding to os-release for GRUB display

--- build_files/01-base.sh
+++ build_files/01-base.sh
@@ -2,6 +2,20 @@
 
 set -xeuo pipefail
 
+# Customize os-release for Razorfin branding
+# Extract version from existing os-release
+VERSION=$(grep "^VERSION=" /usr/lib/os-release | cut -d'"' -f2 | cut -d' ' -f1)
+OSTREE_VERSION=$(grep "^OSTREE_VERSION=" /usr/lib/os-release | cut -d"'" -f2)
+
+sed -i 's/^NAME=.*/NAME="Razorfin"/' /usr/lib/os-release
+sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Razorfin"/' /usr/lib/os-release
+sed -i "s/^BOOTLOADER_NAME=.*/BOOTLOADER_NAME=\"Razorfin (${OSTREE_VERSION})\"/" /usr/lib/os-release
+sed -i 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="razorfin"/' /usr/lib/os-release
+sed -i 's|^HOME_URL=.*|HOME_URL="https://github.com/RazorfinOS-org/Razorfin"|' /usr/lib/os-release
+sed -i 's|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL="https://github.com/RazorfinOS-org/Razorfin"|' /usr/lib/os-release
+sed -i 's|^SUPPORT_URL=.*|SUPPORT_URL="https://github.com/RazorfinOS-org/Razorfin/issues"|' /usr/lib/os-release
+sed -i 's|^BUG_REPORT_URL=.*|BUG_REPORT_URL="https://github.com/RazorfinOS-org/Razorfin/issues"|' /usr/lib/os-release
+
 dnf5 install -y \
     zsh \
     tmux \
